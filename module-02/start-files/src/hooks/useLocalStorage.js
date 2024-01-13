c93d00576da124d92e1cd2034291705667ff2e0c
dykyys <56254283+dykyys@users.noreply.github.com>
2024-01-13 13:01:15 +0300

    Update useLocalStorage.js

--- module-02/start-files/src/hooks/useLocalStorage.js
+++ module-02/start-files/src/hooks/useLocalStorage.js
@@ -2,7 +2,7 @@
 
 export const useLocalStorage = () => {
   // Тут повинен бути власний хук для збереження і отримання даних з localStorage
-  // Даний хук повинен отримувати key, defaultValue і повертати дані з localStorage
-  //якщо дані були збережені
-  //якщо в localStorage не були збережені дані з ключом key, то хук повинен повертати defaultValue
+  // Даний хук повинен отримувати key, defaultValue і повертати дані з localStorage,
+  // якщо дані були збережені.
+  // Якщо в localStorage не були збережені дані з ключем key, то хук повинен повертати defaultValue.
 };
